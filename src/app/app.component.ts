008adb59c7c2cb4a3560afde3c9f9b8dcdec729b
npkumar-sinch <prasanna.kumar@sinch.com>
2023-03-31 20:07:30 +0530

    SSO Tab- ANGULAR(In Progress)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as microsoftTeams from '@microsoft/teams-js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-tab-app';
-}
+  constructor() {
+    microsoftTeams.initialize();
+    microsoftTeams.authentication.getAuthToken({
+      successCallback: (token) => {
+        console.log('Token:', token);
+      },
+      failureCallback: (error) => {
+        console.error('SSO login failed:', error);
+      }
+    });
+    
+  }
+
+  login(){
+    
+    microsoftTeams.authentication.authenticate({
+      // url: 'https://" + window.location.host',
+      url: 'https://a2bc-2001-4490-4c89-8792-945d-4829-eea2-1122.in.ngrok.io/',
+      // width: 600,
+      // height: 535,
+      successCallback: (result: any) => {
+        console.log('SSO login succeeded:', result);
+      },
+      failureCallback: (error: any) => {
+        console.error('SSO login failed:', error);
+      }
+    });
+  }
+  }
